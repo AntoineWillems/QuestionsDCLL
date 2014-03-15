2e11f54dc408c1ea80a75b3978e58216ad168e04
Julien WALLART <julien.wallart@phelsuma.fr>
2014-03-15 12:38:34 +0100

    * Tests sur GiftQuestionService

--- src/main/java/dcll/kowa/ProjetQuestions/App.java
+++ src/main/java/dcll/kowa/ProjetQuestions/App.java
@@ -1,13 +1,38 @@
 package dcll.kowa.ProjetQuestions;
 
+import java.io.IOException;
+
+import org.apache.log4j.BasicConfigurator;
+
+import dcll.kowa.ProjetQuestions.impl.gift.GiftQuestionService;
+import dcll.kowa.ProjetQuestions.impl.gift.GiftReaderException;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws GiftReaderException, IOException
     {
-        System.out.println( "Hello World!" );
+    	BasicConfigurator.configure();
+    	
+    	try
+    	{
+    		GiftQuestionService questionService = new GiftQuestionService();
+        
+    		String maQuestion = "{Sélectionnez les langages dynamiques | type=\"[]\"}"
+    				+ "+ Clojure."
+    				+ "- Java."
+    				+ "+ Groovy."
+    				+ "- Scala.";
+    		
+    		Question parsedQuestion = questionService.getQuestionFromGiftText(maQuestion);
+    		System.out.println(parsedQuestion.getTitle());
+    	}
+    	catch(Exception e)
+    	{
+    		System.out.println(e.toString());
+    	}
     }
 }
