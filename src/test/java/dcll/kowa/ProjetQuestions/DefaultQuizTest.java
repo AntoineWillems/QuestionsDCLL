3dfd9dfb54989dbe379741170646e8c19bae26ab
Dreu <khadrygassama@gmail.com>
2014-03-16 23:15:09 +0100

    Test DefaultQuestion et DefaultQuiz Debut

---
+++ src/test/java/dcll/kowa/ProjetQuestions/DefaultQuizTest.java
@@ -0,0 +1,28 @@
+package dcll.kowa.ProjetQuestions;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import dcll.kowa.ProjetQuestions.impl.DefaultQuestion;
+
+public class DefaultQuizTest {
+
+	@Test
+	public void testaddQuestion(){
+		Question qq = new DefaultQuestion();
+		List<Question> questionList = new ArrayList<Question>();
+		questionList.add(qq);
+		assertEquals(questionList.size(),1);
+	}
+	@Test
+	public void testgetQuestionList(){
+		
+		Question qq = new DefaultQuestion();
+		List<Question> blockList = new ArrayList<Question>();
+	      assertEquals(blockList,qq.getBlockList());
+	}
+}
