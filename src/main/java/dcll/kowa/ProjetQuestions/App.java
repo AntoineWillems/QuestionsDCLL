d2a22134176da38a7264cec87098557b2a9d8e2e
AntoineWillems <willemsan.antoine@gmail.com>
2014-03-17 11:09:36 +0100

    App

--- src/main/java/dcll/kowa/ProjetQuestions/App.java
+++ src/main/java/dcll/kowa/ProjetQuestions/App.java
@@ -1,13 +1,21 @@
 package dcll.kowa.ProjetQuestions;
 
+import java.io.IOException;
+
+import org.apache.log4j.BasicConfigurator;
+
+import dcll.kowa.ProjetQuestions.impl.gift.GiftQuestionService;
+import dcll.kowa.ProjetQuestions.impl.gift.GiftReaderException;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws GiftReaderException, IOException
     {
-        System.out.println( "Hello World!" );
+    	System.out.println( "Hello World!" );
+    	
     }
 }
